a55da2389498f6b618bae716880b7b1e320fe6e1
walterxavierbt <walterxavierbt@outlook.com>
2021-05-11 17:10:12 -0500

    add group creation

--- grantusers.sh
+++ grantusers.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
-while getopts "u:r:s:t:" flag
+while getopts "u:r:s:t:g:" flag
 do
     case "${flag}" in
         u) users=(${OPTARG});; #Separeted username by blank space
         r) role=${OPTARG};;
         s) subscription=${OPTARG};;
         t) tenant=${OPTARG};;
+        g) group=${OPTARG};;
     esac
 done
 
@@ -14,6 +15,12 @@ done
 echo "Checking if subscription already exist"
 #existsubscription= true && $(az account list --query '[0].{Name:name}')
 existsubscription=true
+
+echo "Create Management Group for the project"
+
+    az ad group create --display-name "DEMOGROUP" --mail-nickname "DEMOGROUP"
+    #todo GET THE ID OF THE GROUP
+    az role assignment create --role "$role" --assignee "9a7e39ad-490b-451f-8701-b7b3ba266d18" --subscription "$subscription"
 
 
 if [ "$existsubscription" = true ]
--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ echo "Login Azure using credentials ..........."
 #az login --service-principal --username APP_ID --password PASSWORD --tenant TENANT_ID
 
 echo "Creating New Subscription..........."
-sh ./createsubscription.sh -n "subscriptionaliasname" -w "workload(production, devtest, workload)" -b "billingid"
+#sh ./createsubscription.sh -n "subscriptionaliasname" -w "workload(production, devtest, workload)" -b "billingid"
 
 echo "Creating New Service Ppal"
-sh ./createsp.sh -s "$SUBSCRIPTIONID" -n "$PROJECTNAME"
+#sh ./createsp.sh -s "$SUBSCRIPTIONID" -n "$PROJECTNAME"
 
 echo "Assign Role to ussers in new Subscription"
 sh ./grantusers.sh -u "walxar" -r "Owner" -s "ff14d8b2-4d78-4128-a4a2-192b1b37d9d8" -t "$TENANTDOMAIN"
