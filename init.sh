f7ea5394be2bb68c4bac8b7ea3f3eebce3fb1224
walterxavierbt <walterxavierbt@outlook.com>
2021-05-12 08:45:39 -0500

    Change sh execution to source

--- init.sh
+++ init.sh
@@ -4,16 +4,15 @@ SUBSCRIPTIONID="ff14d8b2-4d78-4128-a4a2-192b1b37d9d8"
 PROJECTNAME="magicadds8"
 TENANTDOMAIN="@walterxavierbtoutlook.onmicrosoft.com"
 
-echo "Login Azure using credentials ..........."
-#az login --service-principal --username APP_ID --password PASSWORD --tenant TENANT_ID
+source ./login.sh
 
 echo "Creating New Subscription..........."
-#sh ./createsubscription.sh -n "subscriptionaliasname" -w "workload(production, devtest, workload)" -b "billingid"
+#source ./createsubscription.sh -n "subscriptionaliasname" -w "workload(production, devtest, workload)" -b "billingid"
 
 echo "Creating New Service Ppal"
-#sh ./createsp.sh -s "$SUBSCRIPTIONID" -n "$PROJECTNAME"
+#source ./createsp.sh -s "$SUBSCRIPTIONID" -n "$PROJECTNAME"
 
 echo "Assign Role to ussers in new Subscription"
-sh ./grantusers.sh -u "walxar" -r "Owner" -s "ff14d8b2-4d78-4128-a4a2-192b1b37d9d8" -t "$TENANTDOMAIN"
+source ./grantusers.sh -u "walxar" -r "Owner" -s "ff14d8b2-4d78-4128-a4a2-192b1b37d9d8" -t "$TENANTDOMAIN"
 
 
