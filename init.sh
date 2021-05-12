211d50a47aca782eceb68c95b316572381146de5
walterxavierbt <walterxavierbt@outlook.com>
2021-05-12 09:01:15 -0500

    add login scripts

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash 
 
-SUBSCRIPTIONID="ff14d8b2-4d78-4128-a4a2-192b1b37d9d8"
-PROJECTNAME="magicadds8"
-TENANTDOMAIN="@walterxavierbtoutlook.onmicrosoft.com"
+echo "loading variables .........................."
+source ./variables.sh
 
 source ./login.sh
 
@@ -10,7 +9,7 @@ echo "Creating New Subscription..........."
 #source ./createsubscription.sh -n "subscriptionaliasname" -w "workload(production, devtest, workload)" -b "billingid"
 
 echo "Creating New Service Ppal"
-#source ./createsp.sh -s "$SUBSCRIPTIONID" -n "$PROJECTNAME"
+#source ./createsp.sh -s "$SUBSCRIPTION_ID" -n "$PROJECTNAME"
 
 echo "Assign Role to ussers in new Subscription"
 source ./grantusers.sh -u "walxar" -r "Owner" -s "ff14d8b2-4d78-4128-a4a2-192b1b37d9d8" -t "$TENANTDOMAIN"
--- login.sh
+++ login.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 
-APP_ID="f825f6dd-b211-46b0-aaf6-5d9474ad099a"
-PASSWORD="8iPidAY189GktzxiAuQVnX-6BZpOTNkhsG"
-TENANT_ID="fedc3089-4e9e-4945-924f-70be916da72b"
-SUBSCRIPTION_ID="ff14d8b2-4d78-4128-a4a2-192b1b37d9d8"
+
 
 echo "Login Azure using credentials ..........."
 az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
