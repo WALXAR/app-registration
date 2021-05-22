f88f502ee334b9a073427cb7e5d9ac0cdd64f6b1
walterxavierbt <walterxavierbt@outlook.com>
2021-05-22 14:18:22 -0500

    add pattern copy scripts

---
+++ gh_patternupload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir /repoa && cd repoa
+echo "cloning repo A"
+git clone git@github.com:WALXAR/github-actions-for-ci.git
+sudo rm -r /c/Users/walte/Desktop/test/github-actions-for-ci.git/.git
+# echo "cloning repo B"
+# git clone git@github.com:WALXAR/demorepo-borrar.git
+# #echo "More description " >> /c/Users/walte/Desktop/test/security-strategy-essentials/README.md
+# cp -r /c/Users/walte/Desktop/test/github-actions-for-ci/* /c/Users/walte/Desktop/test/demorepo-borrar
+# cd /c/Users/walte/Desktop/test/demorepo-borrar
+# git add .
+# git commit -m "new commit"
+# git push origin main
