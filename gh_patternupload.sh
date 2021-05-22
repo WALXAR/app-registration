4b1cf8aef86176aab013e30fd27abc022f50ce9c
walterxavierbt <walterxavierbt@outlook.com>
2021-05-22 15:40:57 -0500

    sleep

--- gh_patternupload.sh
+++ gh_patternupload.sh
@@ -9,10 +9,12 @@ echo "cloning repo A"
 git clone https://$GITHUB_TOKEN@github.com/WALXAR/github-actions-for-ci.git
 sudo rm -rf ~/repoa/github-actions-for-ci/.git
 sudo rm -rf ~/repoa/github-actions-for-ci/.github && cd ~/repoa/github-actions-for-ci && ls -a
+sleep 5
 echo "cloning repo B"
 sudo mkdir ~/repob && cd ~/repob
 sudo chmod 777 ~/repob
 git clone https://$GITHUB_TOKEN@github.com/WALXAR/demorepo-borrar.git && ls -a
+sleep 5
 # #echo "More description " >> /c/Users/walte/Desktop/test/security-strategy-essentials/README.md
 cp -a ~/repoa/github-actions-for-ci/. ~/repob/demorepo-borrar/
 cd ~/repob/demorepo-borrar
