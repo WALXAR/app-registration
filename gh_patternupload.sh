c010d32242f53625c7560d213294461ab74b96b6
walterxavierbt <walterxavierbt@outlook.com>
2021-05-22 14:45:20 -0500

    test 3

--- gh_patternupload.sh
+++ gh_patternupload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo mkdir /repoa && cd repoa
+sudo mkdir /repoa && cd /repoa
 echo "cloning repo A"
 git clone https://$GITHUB_TOKEN@github.com/WALXAR/github-actions-for-ci.git
-sudo rm -r /c/Users/walte/Desktop/test/github-actions-for-ci.git/.git
-ll
+sudo rm -r /repoa/github-actions-for-ci.git/.git
+ls -a
 # echo "cloning repo B"
 # git clone git@github.com:WALXAR/demorepo-borrar.git
 # #echo "More description " >> /c/Users/walte/Desktop/test/security-strategy-essentials/README.md
