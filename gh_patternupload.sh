6acb31773ecd85e3ef1d42c61e29b80538b06ec0
walterxavierbt <walterxavierbt@outlook.com>
2021-05-22 15:17:49 -0500

    change work folder

--- gh_patternupload.sh
+++ gh_patternupload.sh
@@ -3,21 +3,21 @@
 git config --global user.email "walxar@gmail.com"
 git config --global user.name "walxar"
 ls -a
-sudo mkdir ./repoa && cd ./repoa
-sudo chmod 777 ../repoa
+sudo mkdir ~/repoa && cd ~/repoa
+sudo chmod 777 ~/repoa
 echo "cloning repo A"
 git clone https://$GITHUB_TOKEN@github.com/WALXAR/github-actions-for-ci.git
-sudo rm -rf ./repoa/github-actions-for-ci/.git
-sudo rm -rf ./repoa/github-actions-for-ci/.github 
+sudo rm -rf ~/repoa/github-actions-for-ci/.git
+sudo rm -rf ~/repoa/github-actions-for-ci/.github 
 ls -a
 echo "cloning repo B"
-sudo mkdir ../repob && cd ../repob
-sudo chmod 777 ../repob
+sudo mkdir ~/repob && cd ~/repob
+sudo chmod 777 ~/repob
 git clone https://$GITHUB_TOKEN@github.com/WALXAR/demorepo-borrar.git
 ls -a
 # #echo "More description " >> /c/Users/walte/Desktop/test/security-strategy-essentials/README.md
-cp -r ../repoa/github-actions-for-ci/ ./repob/demorepo-borrar/
-cd ./demorepo-borrar
+cp -r ~/repoa/github-actions-for-ci/ ~/repob/demorepo-borrar/
+cd ~/repob/demorepo-borrar
 ls -a
 git add .
 git commit -m "new commit"
