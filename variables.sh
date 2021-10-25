c2564cc0c759a5107451c45dd77a961b48677adb
WALXAR <46756209+WALXAR@users.noreply.github.com>
2021-10-25 15:29:37 -0500

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -8,7 +8,7 @@ set -a
 SUBSCRIPTION_NAME="Sandbox$PROJECTNAME"
 SUBSCRIPTION_DISPLAY_NAME="AMR.BPG.Sandbox.$PROJECTNAME"
 GROUP_NAME="$PROJECTNAME Admins"
-#****************************************************************************
+#****************************************************************************************
 
 
 set +a 
