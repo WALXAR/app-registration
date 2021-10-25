e2dec8cc28eb48fbb903d50eae77608b7d13e273
WALXAR <46756209+WALXAR@users.noreply.github.com>
2021-10-25 15:33:39 -0500

    AB#3
    AB#3

--- variables.sh
+++ variables.sh
@@ -4,7 +4,7 @@ set -a
 
 
 # Project Configuration 
-#*******************************************************************************
+#****************************************************************************************
 SUBSCRIPTION_NAME="Sandbox$PROJECTNAME"
 SUBSCRIPTION_DISPLAY_NAME="AMR.BPG.Sandbox.$PROJECTNAME"
 GROUP_NAME="$PROJECTNAME Admins"
